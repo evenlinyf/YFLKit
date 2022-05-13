323cc5d8d27fdddbf81388761ac96bef64c22381
Even Lin <evenlinyf@gmail.com>
2022-05-13 15:20:02 +0800

    调整日志时间

--- YFLKit/Tool/YFLogger.swift
+++ YFLKit/Tool/YFLogger.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public func YFLog<T>(_ message: T, file: String = #file, method: String = #function, line: Int = #line) {
 #if DEBUG
-    let time = Date().yfl.timeString("yyyy-MM-dd HH:mm:ss")
+    let time = Date().yfl.timeString("yyyy-MM-dd HH:mm:ss.SSS")
     let finalString = "YFLog \(time)\n\((file as NSString).lastPathComponent)[\(line)],\(method):\n\(message)"
     print(finalString)
 #endif
